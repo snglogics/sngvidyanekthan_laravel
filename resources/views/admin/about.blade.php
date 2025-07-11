c7b022734711ac4c2380d9dff320fc0c3ba7a6c1
SARATHLAL HS <sarathlalsh@gmail.com>
2025-07-11 15:12:31 +0530

    pta members adding to admin

--- resources/views/admin/about.blade.php
+++ resources/views/admin/about.blade.php
@@ -3,31 +3,48 @@
 
 
 @section('content')
-<div class="container py-5">
-    <h2 class="text-center mb-5 fw-bold text-primary">About</h2>
-    <div class="row g-4 justify-content-center homepage-buttons">
+    <div class="container py-5">
+        <h2 class="text-center mb-5 fw-bold text-primary">About</h2>
+        <div class="row g-4 justify-content-center homepage-buttons">
 
-        @php
-            $buttons = [
-                ['route' => 'admin.events.create', 'label' => 'Upcoming Events',  'icon' => 'fas fa-calendar-check', 'color' => 'info'],
-                ['route' => 'admin.campus-overviews.index', 'label' => 'Campus overviews', 'icon' => 'fas fa-university', 'color' => 'info'],
-                 ];
-        @endphp
+            @php
+                $buttons = [
+                    [
+                        'route' => 'admin.events.create',
+                        'label' => 'Upcoming Events',
+                        'icon' => 'fas fa-calendar-check',
+                        'color' => 'info',
+                    ],
+                    [
+                        'route' => 'admin.campus-overviews.index',
+                        'label' => 'Campus overviews',
+                        'icon' => 'fas fa-university',
+                        'color' => 'info',
+                    ],
+                    [
+                        'route' => 'admin.pta-members.index',
+                        'label' => 'PTA Members',
+                        'icon' => 'fas fa-users',
+                        'color' => 'info',
+                    ],
+                ];
+            @endphp
 
-        @foreach($buttons as $btn)
-        <div class="col-md-3 col-sm-6" data-aos="zoom-in">
-            <div class="card h-100 text-center shadow-sm border-0 bg-light">
-                <div class="card-body d-flex flex-column align-items-center justify-content-center py-4">
-                    <i class="{{ $btn['icon'] }} fa-3x text-{{ $btn['color'] }} mb-3"></i>
-                    <h5 class="card-title">{{ $btn['label'] }}</h5>
-                    <button onclick="window.location.href='{{ route($btn['route']) }}'" class="btn btn-outline-{{ $btn['color'] }} mt-2 w-100">
-                        Go
-                    </button>
+            @foreach ($buttons as $btn)
+                <div class="col-md-3 col-sm-6" data-aos="zoom-in">
+                    <div class="card h-100 text-center shadow-sm border-0 bg-light">
+                        <div class="card-body d-flex flex-column align-items-center justify-content-center py-4">
+                            <i class="{{ $btn['icon'] }} fa-3x text-{{ $btn['color'] }} mb-3"></i>
+                            <h5 class="card-title">{{ $btn['label'] }}</h5>
+                            <button onclick="window.location.href='{{ route($btn['route']) }}'"
+                                class="btn btn-outline-{{ $btn['color'] }} mt-2 w-100">
+                                Go
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        @endforeach
+            @endforeach
 
+        </div>
     </div>
-</div>
 @endsection
